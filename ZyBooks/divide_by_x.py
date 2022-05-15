f5495b2075af67102d6f4d50b72e347ca1823ea8
marshburngm <100003121+marshburngm@users.noreply.github.com>
2022-05-15 07:18:38 -0400

    Create divide_by_x.py

---
+++ ZyBooks/divide_by_x.py
@@ -0,0 +1,11 @@
+#Write a program using integers user_num and x as input, and output user_num divided by x three times.
+
+user_num = input()
+
+x = input()
+
+user_num2 = int(user_num)//int(x)
+user_num3 = int(user_num)//int(x)**2
+user_num4 = int(user_num)//int(x)**3
+
+print(user_num2,user_num3,user_num4)
