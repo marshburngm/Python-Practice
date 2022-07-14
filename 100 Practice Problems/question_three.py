51d9ca9fd3764b31cf238db9963737746ac9c1af
marshburngm <100003121+marshburngm@users.noreply.github.com>
2022-07-14 09:04:40 -0400

    Delete 100 Practice Problems directory

--- 100 Practice Problems/question_three.py
+++
@@ -1,9 +0,0 @@
-#With a given integral number n, write a program to generate a dictionary that contains (i, i x i) such that is an integral number between 1 and n (both included). 
-#And then the program should print the dictionary.
-
-def showSquared(num):
-    squared = {}
-    for i in range(1, num+1):
-        squared[i] = i*i
-    print(squared)
-    
